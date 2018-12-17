9deb85a0e278041b7807b3f5aff15d0a2a91e79c
Deadpikle <mbabienco@southern.edu>
2018-12-17 15:18:29 +0700

    Add GeneratedBarcodes model

---
+++ SimpleInventory/Models/GeneratedBarcodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleInventory.Models
+{
+    class GeneratedBarcode
+    {
+        public int ID { get; set; }
+        public string Number { get; set; }
+
+        public DateTime DateTimeGenerated { get; set; }
+        public int GeneratedByUserID { get; set; }
+    }
+}
