d7a84d60484272ec3e18eb8ef31a87f273bf872c
Deadpikle <mbabienco@southern.edu>
2018-12-17 14:40:59 +0700

    Add a bunch of packages and stub out some classes

---
+++ SimpleInventory/Helpers/BarcodeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarcodeLib;
+
+
+namespace SimpleInventory.Helpers
+{
+    class BarcodeGenerator
+    {
+        public void foo()
+        {
+            // example from BarcodeLib docs:
+            // BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+            // Image img = b.Encode(BarcodeLib.TYPE.UPCA, "038000356216", Color.Black, Color.White, 290, 120);
+        }
+        
+    }
+}
---
+++ SimpleInventory/Helpers/BarcodePDFGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventory.Helpers
+{
+    // class for generating a PDF of X number of barcodes via PDFSharp and BarcodeLib
+    class BarcodePDFGenerator
+    {
+    }
+}
