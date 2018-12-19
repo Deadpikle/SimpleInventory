e3277fb8f21fb5a5eafad60ed651152e47785209
Deadpikle <mbabienco@southern.edu>
2018-12-19 15:06:48 +0700

    Setup data binding for scan items page

---
+++ SimpleInventory/Helpers/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventory.Helpers
+{
+    class Utilities
+    {
+        public static bool InDesignMode()
+        {
+            return  LicenseManager.UsageMode == LicenseUsageMode.Designtime;
+        }
+    }
+}
--- SimpleInventory/ViewModels/ScanItemsViewModel.cs
+++ SimpleInventory/ViewModels/ScanItemsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleInventory.Helpers;
 using SimpleInventory.Interfaces;
+using SimpleInventory.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,80 @@ namespace SimpleInventory.ViewModels
 {
     class ScanItemsViewModel : BaseViewModel
     {
+        private string _barcodeNumber;
+        private InventoryItem _purchasedItem;
+        private ItemSoldInfo _itemSoldInfo;
+        private bool _purchaseInfoIsVisible;
+
+        private List<Currency> _currencies;
+        private string _itemPurchaseStatusMessage;
+        private int _selectedPaidCurrencyIndex;
+        private string _changeNeeded;
+        private int _selectedChangeCurrencyIndex;
+
         public ScanItemsViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
         {
+            _currencies = Currency.LoadCurrencies();
+            PurchasedItem = null;
+            PurchaseInfo = null;
+            PurchaseInfoIsVisible = false;
+            ItemPurchaseStatusMessage = "";
+            SelectedPaidCurrencyIndex = -1;
         }
+
+        #region Properties
+
+        public string BarcodeNumber
+        {
+            get { return _barcodeNumber; }
+            set { _barcodeNumber = value; NotifyPropertyChanged(); }
+        }
+
+        public InventoryItem PurchasedItem
+        {
+            get { return _purchasedItem; }
+            set { _purchasedItem = value; NotifyPropertyChanged(); }
+        }
+
+        public ItemSoldInfo PurchaseInfo
+        {
+            get { return _itemSoldInfo; }
+            set { _itemSoldInfo = value; NotifyPropertyChanged(); }
+        }
+
+        public bool PurchaseInfoIsVisible
+        {
+            get { return _purchaseInfoIsVisible; }
+            set { _purchaseInfoIsVisible = value; NotifyPropertyChanged(); }
+        }
+
+        public string ItemPurchaseStatusMessage
+        {
+            get { return _itemPurchaseStatusMessage; }
+            set { _itemPurchaseStatusMessage = value; NotifyPropertyChanged(); }
+        }
+
+        public int SelectedPaidCurrencyIndex
+        {
+            get { return _selectedPaidCurrencyIndex; }
+            set { _selectedPaidCurrencyIndex = value; NotifyPropertyChanged(); }
+        }
+
+        public string ChangeNeeded
+        {
+            get { return _changeNeeded; }
+            set { _changeNeeded = value; NotifyPropertyChanged(); }
+        }
+
+        public int SelectedChangeCurrencyIndex
+        {
+            get { return _selectedChangeCurrencyIndex; }
+            set { _selectedChangeCurrencyIndex = value; NotifyPropertyChanged(); }
+        }
+
+        #endregion
+
+        #region ICommands
 
         public ICommand GoToMainMenu
         {
@@ -34,5 +106,17 @@ namespace SimpleInventory.ViewModels
         {
 
         }
+
+        public ICommand SavePurchaseUpdates
+        {
+            get { return new RelayCommand(SavePurchaseUpdatesToDB); }
+        }
+
+        private void SavePurchaseUpdatesToDB()
+        {
+
+        }
+
+        #endregion
     }
 }
