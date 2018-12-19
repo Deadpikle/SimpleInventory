1a7328ebb7eb37618c9b2bb94a225a078e192dbd
Deadpikle <mbabienco@southern.edu>
2018-12-19 17:21:35 +0700

    Add page for generating barcodes

---
+++ SimpleInventory/ViewModels/GenerateBarcodesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SimpleInventory.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventory.ViewModels
+{
+    class GenerateBarcodesViewModel : BaseViewModel
+    {
+        public GenerateBarcodesViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
+        {
+        }
+    }
+}
