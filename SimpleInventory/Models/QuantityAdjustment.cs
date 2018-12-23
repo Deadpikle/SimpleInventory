23101907295f173f832138676b1553e9c2cdb1b0
Deadpikle <mbabienco@southern.edu>
2018-12-23 14:29:31 +0700

    Add QuantityAdjustment.UpdateQuantity

--- SimpleInventory/Models/QuantityAdjustment.cs
+++ SimpleInventory/Models/QuantityAdjustment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleInventory.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,25 @@ namespace SimpleInventory.Models
         DateTime DateTimeChanged { get; set; }
         int InventoryItemID { get; set; }
         int UserID { get; set; }
+
+        public static void UpdateQuantity(int quantity, int itemID, int userID)
+        {
+            var dbHelper = new DatabaseHelper();
+            using (var conn = dbHelper.GetDatabaseConnection())
+            {
+                using (var command = dbHelper.GetSQLiteCommand(conn))
+                {
+                    string query = "INSERT INTO QuantityAdjustments (AmountChanged, DateTimeChanged, InventoryItemID, UserID)" +
+                        " VALUES (@amount, @dateTime, @itemID, @userID);";
+                    command.CommandText = query;
+                    command.Parameters.AddWithValue("@amount", quantity);
+                    command.Parameters.AddWithValue("@dateTime", DateTime.Now.ToString(Utilities.DateTimeToStringFormat()));
+                    command.Parameters.AddWithValue("@itemID", itemID);
+                    command.Parameters.AddWithValue("@userID", userID);
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
     }
 }
