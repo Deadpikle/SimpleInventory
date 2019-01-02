a1c4d263b1cb3ea5fc08324c754f8a94f6060a29
Deadpikle <mbabienco@southern.edu>
2019-01-02 18:09:01 +0700

    Day reports now handle multiple pages (except for total rows)

--- SimpleInventory/Helpers/DayReportPDFGenerator.cs
+++ SimpleInventory/Helpers/DayReportPDFGenerator.cs
@@ -16,28 +16,18 @@ namespace SimpleInventory.Helpers
 {
     class DayReportPDFGenerator
     {
-        public void GeneratePDF(DaySales sales, string outputPath)
+        private void AddTitle(DaySales sales, XUnit margin, PdfPage page, XGraphics gfx)
         {
-            PdfDocument document = new PdfDocument();
-            document.Info.Title = "Inventory Daily Report -- " + sales.Date.ToString("yyyy-MM-dd");
-
-
-            PdfPage page = document.AddPage();
-            page.Size = PdfSharp.PageSize.A4;
-
-            XGraphics gfx = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
-            XUnit margin = XUnit.FromInch(1);
             XUnit yCoord = margin;
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
             gfx.DrawString("Inventory Daily Report", font, XBrushes.Black,
                 new XRect(0, yCoord, page.Width, page.Height), XStringFormats.TopCenter);
             gfx.DrawString(sales.Date.ToString("d MMMM, yyyy"), font, XBrushes.Black,
                 new XRect(0, yCoord + XUnit.FromInch(0.4), page.Width, page.Height), XStringFormats.TopCenter);
+        }
 
-
-            yCoord += XUnit.FromInch(1.3);
-
-            // draw headers
+        private void DrawHeaders(XUnit yCoord, XUnit margin, XGraphics gfx)
+        {
             XUnit xCoord = margin;
             XFont headerFont = new XFont("Verdana", 16, XFontStyle.Bold);
             gfx.DrawString("Name", headerFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
@@ -47,21 +37,61 @@ namespace SimpleInventory.Helpers
             gfx.DrawString("Income", headerFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
             xCoord += XUnit.FromInch(1.5);
             gfx.DrawString("Profit", headerFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
+        }
 
+        private void DrawPageNumber(int number, XUnit margin, PdfPage page, XGraphics gfx)
+        {
+            XFont headerFont = new XFont("Verdana", 10, XFontStyle.Regular);
+            gfx.DrawString("-" + number.ToString() + "-", headerFont, XBrushes.Black, new XRect(0, page.Height - margin, page.Width, margin), XStringFormats.Center);
+        }
+
+        public void GeneratePDF(DaySales sales, string outputPath)
+        {
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Inventory Daily Report -- " + sales.Date.ToString("yyyy-MM-dd");
+
+            PdfPage page = document.AddPage();
+            page.Size = PdfSharp.PageSize.A4;
+
+            XUnit margin = XUnit.FromInch(1);
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            AddTitle(sales, margin, page, gfx);
+            int pageNumber = 1;
+            DrawPageNumber(pageNumber, margin, page, gfx);
+
+            XUnit yCoord = margin + XUnit.FromInch(1.3);
+
+            // draw headers
+            DrawHeaders(yCoord, margin, gfx);
+
+            XUnit xCoord = margin;
             XFont itemFont = new XFont("Segoe UI", 14, XFontStyle.Regular);
 
-            foreach (ReportItemSold itemSold in sales.ItemsSold)
-            {
-                yCoord += XUnit.FromInch(0.3);
-                xCoord = margin;
-                gfx.DrawString(itemSold.Name, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
-                xCoord += XUnit.FromInch(2.5);
-                gfx.DrawString(itemSold.QuantityPurchased.ToString(), itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
-                xCoord += XUnit.FromInch(1.5);
-                gfx.DrawString(itemSold.TotalCostWithCurrency, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
-                xCoord += XUnit.FromInch(1.5);
-                gfx.DrawString(itemSold.TotalProfitWithCurrency, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
-            }
+            //for (int i = 0; i < 40; i++) // for creating lots of PDF dummy data
+            //{
+                foreach (ReportItemSold itemSold in sales.ItemsSold)
+                {
+                    if (yCoord + XUnit.FromInch(0.4) >= page.Height - margin)
+                    {
+                        page = document.AddPage();
+                        page.Size = PdfSharp.PageSize.A4;
+                        gfx = XGraphics.FromPdfPage(page);
+                        AddTitle(sales, margin, page, gfx);
+                        DrawPageNumber(++pageNumber, margin, page, gfx);
+                        yCoord = margin + XUnit.FromInch(1.3);
+                        DrawHeaders(yCoord, margin, gfx);
+                    }
+                    yCoord += XUnit.FromInch(0.3);
+                    xCoord = margin;
+                    gfx.DrawString(itemSold.Name, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
+                    xCoord += XUnit.FromInch(2.5);
+                    gfx.DrawString(itemSold.QuantityPurchased.ToString(), itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
+                    xCoord += XUnit.FromInch(1.5);
+                    gfx.DrawString(itemSold.TotalCostWithCurrency, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
+                    xCoord += XUnit.FromInch(1.5);
+                    gfx.DrawString(itemSold.TotalProfitWithCurrency, itemFont, XBrushes.Black, new XPoint(xCoord, yCoord), XStringFormats.CenterLeft);
+                }
+            //}
             // print totals
             yCoord += XUnit.FromInch(0.5);
             XFont totalFont = new XFont("Segoe UI", 16, XFontStyle.Bold);
