8dc98886a518a15adaa4468bb2dc7522cfa29fd4
Deadpikle <mbabienco@southern.edu>
2019-01-02 16:53:19 +0700

    Show currency on sales reports

--- SimpleInventory/Models/ReportItemSold.cs
+++ SimpleInventory/Models/ReportItemSold.cs
@@ -12,9 +12,59 @@ namespace SimpleInventory.Models
         public decimal QuantityPurchased { get; set; }
         public decimal CostPerItem { get; set; }
         public Currency CostCurrency { get; set; }
+
+        public string CostWithCurrency
+        {
+            get
+            {
+                if (CostCurrency != null)
+                {
+                    return CostPerItem.ToString() + " (" + CostCurrency?.Symbol + ")";
+                }
+                return CostPerItem.ToString();
+            }
+        }
+
         public decimal TotalCost { get; set; }
+
+        public string TotalCostWithCurrency
+        {
+            get
+            {
+                if (CostCurrency != null)
+                {
+                    return TotalCost.ToString() + " (" + CostCurrency?.Symbol + ")";
+                }
+                return TotalCost.ToString();
+            }
+        }
+
         public decimal ProfitPerItem { get; set; }
         public Currency ProfitCurrency { get; set; }
         public decimal TotalProfit { get; set; }
+
+        public string ProfitWithCurrency
+        {
+            get
+            {
+                if (ProfitCurrency != null)
+                {
+                    return ProfitPerItem.ToString() + " (" + ProfitCurrency?.Symbol + ")";
+                }
+                return ProfitPerItem.ToString();
+            }
+        }
+
+        public string TotalProfitWithCurrency
+        {
+            get
+            {
+                if (ProfitCurrency != null)
+                {
+                    return TotalProfit.ToString() + " (" + ProfitCurrency?.Symbol + ")";
+                }
+                return TotalProfit.ToString();
+            }
+        }
     }
 }
