536f58ba2408f5aa03155589d3e1afc23c9e330e
Deadpikle <mbabienco@southern.edu>
2019-01-09 18:49:12 +0700

    Start working on editing item types

---
+++ SimpleInventory/ViewModels/CreateOrEditItemTypeViewModel.cs
@@ -0,0 +1,43 @@
+﻿using SimpleInventory.Helpers;
+using SimpleInventory.Interfaces;
+using SimpleInventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleInventory.ViewModels
+{
+    class CreateOrEditItemTypeViewModel : BaseViewModel
+    {
+        private string _screenTitle;
+
+        public CreateOrEditItemTypeViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
+        {
+            ScreenTitle = "Add Item Type";
+        }
+
+        public CreateOrEditItemTypeViewModel(IChangeViewModel viewModelChanger, ItemType itemType) : base(viewModelChanger)
+        {
+            ScreenTitle = "Edit Item Type";
+        }
+
+        public string ScreenTitle
+        {
+            get { return _screenTitle; }
+            set { _screenTitle = value; NotifyPropertyChanged(); }
+        }
+
+        public ICommand ReturnToManageTypes
+        {
+            get { return new RelayCommand(PopScreen); }
+        }
+
+        private void PopScreen()
+        {
+            PopViewModel();
+        }
+    }
+}
---
+++ SimpleInventory/ViewModels/ViewItemTypesViewModel.cs
@@ -0,0 +1,52 @@
+﻿using SimpleInventory.Helpers;
+using SimpleInventory.Interfaces;
+using SimpleInventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleInventory.ViewModels
+{
+    class ViewItemTypesViewModel : BaseViewModel
+    {
+        private List<ItemType> _itemTypes;
+        private int _selectedItemTypeIndex;
+        private ItemType _selectedItem;
+
+        public ViewItemTypesViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
+        {
+            ItemTypes = ItemType.LoadItemTypes();
+        }
+
+        public List<ItemType> ItemTypes
+        {
+            get { return _itemTypes; }
+            set { _itemTypes = value; NotifyPropertyChanged(); }
+        }
+
+        public int SelectedItemTypeIndex
+        {
+            get { return _selectedItemTypeIndex; }
+            set { _selectedItemTypeIndex = value; NotifyPropertyChanged(); }
+        }
+
+        public ItemType SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value; NotifyPropertyChanged(); }
+        }
+
+        public ICommand GoToMainMenu
+        {
+            get { return new RelayCommand(ReturnToPreviousScreen); }
+        }
+
+        private void ReturnToPreviousScreen()
+        {
+            PopViewModel();
+        }
+    }
+}
