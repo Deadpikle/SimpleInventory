d27058b2854e4fe877a6ad9db30c75a4fb0e11d8
Deadpikle <mbabienco@southern.edu>
2019-02-01 11:48:09 +0700

    Add inactivity timer

--- SimpleInventory/ViewModels/MainWindowViewModel.cs
+++ SimpleInventory/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace SimpleInventory.ViewModels
 {
@@ -13,18 +15,43 @@ namespace SimpleInventory.ViewModels
         BaseViewModel _currentViewModel;
         Stack<BaseViewModel> _viewModels;
 
+        // https://stackoverflow.com/a/4970019 -- logic for inactivity
+        private readonly DispatcherTimer _activityTimer;
+
         public MainWindowViewModel()
         {
             _viewModels = new Stack<BaseViewModel>();
             var initialViewModel = new LoginViewModel(this);
             _viewModels.Push(initialViewModel);
             CurrentViewModel = initialViewModel;
+            // setup inactivity timer
+            InputManager.Current.PreProcessInput += InputPreProcessInput;
+            _activityTimer = new DispatcherTimer { Interval = TimeSpan.FromMinutes(10), IsEnabled = true };
+            _activityTimer.Tick += ActivityTimerTick;
         }
 
         public BaseViewModel CurrentViewModel
         {
             get { return _currentViewModel; }
             set { _currentViewModel = value; NotifyPropertyChanged(); }
+        }
+
+        private void ActivityTimerTick(object sender, EventArgs e)
+        {
+            // set UI to inactivity
+            PopToBaseViewModel();
+        }
+
+        private void InputPreProcessInput(object sender, PreProcessInputEventArgs e)
+        {
+            InputEventArgs inputEventArgs = e.StagingItem.Input;
+
+            if (inputEventArgs is MouseEventArgs || inputEventArgs is KeyboardEventArgs)
+            {
+                // reset timer
+                _activityTimer.Stop();
+                _activityTimer.Start();
+            }
         }
 
         #region IChangeViewModel
@@ -38,6 +65,15 @@ namespace SimpleInventory.ViewModels
         public void PopViewModel()
         {
             if (_viewModels.Count > 1)
+            {
+                _viewModels.Pop();
+            }
+            CurrentViewModel = _viewModels.Peek();
+        }
+
+        public void PopToBaseViewModel()
+        {
+            while (_viewModels.Count > 1)
             {
                 _viewModels.Pop();
                 CurrentViewModel = _viewModels.Peek();
