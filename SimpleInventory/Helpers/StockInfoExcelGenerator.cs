47a38a9fb3a15890dd464fac8183c2283d66b8c7
Deadpikle <mbabienco@southern.edu>
2019-04-29 15:11:14 +0700

    Show diff between missing and extra items in excel report

--- SimpleInventory/Helpers/StockInfoExcelGenerator.cs
+++ SimpleInventory/Helpers/StockInfoExcelGenerator.cs
@@ -31,7 +31,8 @@ namespace SimpleInventory.Helpers
                 worksheet.Cell("G4").SetValue("Manual Difference").Style.Font.SetBold(true);
                 worksheet.Cell("H4").SetValue("Stock Difference").Style.Font.SetBold(true);
                 worksheet.Cell("I4").SetValue("Item Cost").Style.Font.SetBold(true);
-                worksheet.Cell("J4").SetValue("Cost Difference").Style.Font.SetBold(true);
+                worksheet.Cell("J4").SetValue("Cost Difference (Missing Items)").Style.Font.SetBold(true);
+                worksheet.Cell("K4").SetValue("Cost Difference (Extra Items)").Style.Font.SetBold(true);
 
                 // start exporting data
                 var currentCell = worksheet.Cell("A5");
@@ -71,9 +72,25 @@ namespace SimpleInventory.Helpers
                         .WhenNotEquals("0")
                         .Fill.SetBackgroundColor(XLColor.LightPink); // stock difference
                     currentCell.CellRight(8).Value = item.Item.Cost; // item cost
-                    currentCell.CellRight(9).SetFormulaA1("=" + 
-                        currentCell.CellRight(7).Address.ToStringFixed() + "*" + 
-                        currentCell.CellRight(8).Address.ToStringFixed()); // cost difference
+                    // first sum column is items that have less in real life than in the computer
+                    string formula = string.Format("=IF({0}<>\"\", IF({1}>{2},{3}*{4},\"\"), \"\")",
+                        currentCell.CellRight(4).Address.ToStringFixed(), // ending stock manual entry
+                        currentCell.CellRight(3).Address.ToStringFixed(), // ending stock computer
+                        currentCell.CellRight(4).Address.ToStringFixed(), // ending stock manual entry
+                        currentCell.CellRight(7).Address.ToStringFixed(), // stock diff
+                        currentCell.CellRight(8).Address.ToStringFixed()  // item cost
+                        );
+                    currentCell.CellRight(9).SetFormulaA1(formula); // cost difference for missing items
+                    // second sum column is items that have more in real life than in the computer
+                    formula = string.Format("=IF({0}<>\"\", IF({1}<{2},{3}*{4},\"\"), \"\")",
+                        currentCell.CellRight(4).Address.ToStringFixed(), // ending stock manual entry
+                        currentCell.CellRight(3).Address.ToStringFixed(), // ending stock computer
+                        currentCell.CellRight(4).Address.ToStringFixed(), // ending stock manual entry
+                        currentCell.CellRight(7).Address.ToStringFixed(), // stock diff
+                        currentCell.CellRight(8).Address.ToStringFixed()  // item cost
+                        );
+                    currentCell.CellRight(10).SetFormulaA1(formula); // cost difference for extra items
+                    // if item count is equal, doesn't add to either column
 
                     if (currentCell.WorksheetRow().RowNumber() % 2 == 0)
                     {
@@ -91,13 +108,15 @@ namespace SimpleInventory.Helpers
                     currentCell.CellRight(8).SetValue("Cost Discrepency").Style.Font.SetBold(true);
                     currentCell.CellRight(9).SetFormulaA1("=SUM(" + firstCellWithData.CellRight(9).Address.ToStringFixed()
                         + ":" + currentCell.CellAbove(1).CellRight(9).Address.ToStringFixed() + ")").Style.Font.SetBold(true);
+                    currentCell.CellRight(10).SetFormulaA1("=SUM(" + firstCellWithData.CellRight(10).Address.ToStringFixed()
+                        + ":" + currentCell.CellAbove(1).CellRight(10).Address.ToStringFixed() + ")").Style.Font.SetBold(true);
                 }
                 //// auto fit width
                 worksheet.Columns().AdjustToContents(4, 4, 10, 25);
                 // set print area
                 worksheet.PageSetup.PrintAreas.Clear();
                 var firstCellForPrinting = worksheet.Cell("A1");
-                var lastCellForPrinting = items.Count > 0 ? currentCell.CellRight(9) : worksheet.Cell("J4");
+                var lastCellForPrinting = items.Count > 0 ? currentCell.CellRight(10) : worksheet.Cell("J4");
                 worksheet.PageSetup.PrintAreas.Add(firstCellForPrinting.Address.ToStringRelative() + ":" + lastCellForPrinting.Address.ToStringRelative());
                 worksheet.PageSetup.SetRowsToRepeatAtTop("4:4");
                 worksheet.PageSetup.PagesWide = 1;
