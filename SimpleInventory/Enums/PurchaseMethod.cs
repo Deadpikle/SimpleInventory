14b4bb5446fe62912ccf3b20795806129f7effc6
Deadpikle <mbabienco@southern.edu>
2023-09-27 08:49:45 -0400

    Add extension method for PurchaseMethod to str

--- SimpleInventory/Enums/PurchaseMethod.cs
+++ SimpleInventory/Enums/PurchaseMethod.cs
@@ -1,8 +1,27 @@
-﻿namespace SimpleInventory.Enums
+﻿using System;
+
+namespace SimpleInventory.Enums
 {
     public enum PurchaseMethod
     {
         Cash = 1,
         QRCode = 2
+    }
+
+    // https://stackoverflow.com/a/5985710
+    static class PurchaseMethodMethods
+    {
+        public static string ToHumanReadableString(this PurchaseMethod pm)
+        {
+            switch (pm)
+            {
+                case PurchaseMethod.Cash:
+                    return "Cash";
+                case PurchaseMethod.QRCode:
+                    return "QR Code";
+                default:
+                    return "Cash";
+            }
+        }
     }
 }
